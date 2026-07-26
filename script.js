0ca09bdc4b7ee681ec37f1e25ae459ed130d2b64
chelseiji <chelseacalalb@gmail.com>
2026-07-26 16:29:16 +0100

    Added the falling flowers

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 console.log('using app');
+
+//This is so that the flowers dont start falling instantly when users open the application
+setTimeout(function() {
+    document.querySelector('.flowerfall').classList.add('active');
+}, 2000);
