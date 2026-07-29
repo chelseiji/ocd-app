ab964130361999a086a3353bfcf7e25d595b8cd1
chelseiji <chelseacalalb@gmail.com>
2026-07-29 18:02:30 +0100

    registration page added

--- script.js
+++ script.js
@@ -40,3 +40,31 @@ function skipOnboarding() {
     closePopup();
     showScreen('authentication'); // change this 
 }
+
+// Changes the password form visible to not visible as well as the eyes
+function togglePassword(inputId, icon) {
+    var input = document.getElementById(inputId);
+
+    if (input.type === 'password') {
+        input.type = 'text';
+        icon.src = 'images/eye-open.svg';
+    } else {
+        input.type = 'password';
+        icon.src = 'images/eye-closed.svg';
+    }
+}
+
+//Sees if the passwords are a match if they are they can go to the terms and conditions page
+function Registration() {
+    var password = document.getElementById('password-input').value;
+    var confirm = document.getElementById('confirm-input').value;
+    var errorText = document.getElementById('error');
+
+    if (password !== confirm) {
+        errorText.classList.add('show');
+        return;
+    }
+
+    errorText.classList.remove('show');
+    showScreen('termsandconditions');
+}
